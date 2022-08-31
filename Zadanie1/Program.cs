55cd2f3d17c513d1757970eec5bc435aa3edccee
Oleg <Oleg.evpator@mail.ru>
2022-08-31 09:59:43 +0300

    add Zadanie1.1

--- Zadanie1/Program.cs
+++ Zadanie1/Program.cs
@@ -1,10 +1,9 @@
-﻿
-Console.WriteLine("Hello, World!");
-
+﻿Console.WriteLine("Hello, World!");
 Console.Write("Введите первое число, ");
 int  number1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число, ");
 int number2 = Convert.ToInt32(Console.ReadLine());
+int min = number1;
 
 if (number1 < number2)
     {
