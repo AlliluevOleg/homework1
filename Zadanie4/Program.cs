7143d955c59be5a6dac4cb960980def7d5e607f6
Oleg <Oleg.evpator@mail.ru>
2022-08-31 11:52:47 +0300

    add Zadanie4

---
+++ Zadanie4/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine("Hello, World!");
+Console.Write("Число а: ");
+ int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Число b: ");
+ int b = Convert.ToInt32(Console.ReadLine());
+
+ int i = a;
+ for (i = a; i <= b; i++)
+{
+    if (i % 2 == 0)
+    Console.Write(i+ " ");
+}
