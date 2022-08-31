323678ff6115aedc9f7cd2c8b22b3be4470331c0
Oleg <Oleg.evpator@mail.ru>
2022-08-31 09:34:57 +0300

    add Zadanie1

---
+++ Zadanie1/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("Hello, World!");
+Console.Write("Введите первое число, ");
+int  number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число, ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 < number2)
+    {
+        Console.WriteLine("number1");
+    }
+else  
+{
+    Console.WriteLine("number2");
+}
